71d2387f01d26bb3d9beb174375f2ee4d855cb8c
hide card <hidecard1500@gmail.com>
2025-11-19 17:08:48 +0630

    Oop Fix

--- oop.php
+++ oop.php
@@ -33,3 +33,57 @@ class Car
 }
 
 ?>
+
+
+
+
+<?php
+class Car
+{
+    private $model;
+    public function getmodel()
+    {
+        return "The car is model : " . $this->model;
+    }
+}
+$mercedes = new Car();
+$mercedes->model = "Mercedes";
+//echo $mercedies->getmodel();
+?>
+
+<?php
+class Myclass
+{
+    private $property;
+    public function __construct($initialValue)
+    {
+        $this->property = $initialValue;
+        echo "Construct called with initialValue : $initialValue\n";
+    }
+    public function getproperty()
+    {
+        return $this->property;
+    }
+}
+$obj = new Myclass("Hello World");
+echo $obj->getproperty();
+?>
+
+<?php
+class Car
+{
+    private $model = "";
+    public function __construct($model = null)
+    {
+        if ($model) {
+            $this->model = $model;
+        }
+    }
+    public function getcarModel()
+    {
+        return "This car model is : " . $this->model;
+    }
+}
+$car1 = new Car();
+echo $car1->getcarModel()
+    ?>
