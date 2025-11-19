df70841df0d7e0c332206f2e83f60d9d4645ed6d
hide card <hidecard1500@gmail.com>
2025-11-19 10:06:45 +0630

    fix day3

--- day3.php
+++ day3.php
@@ -112,3 +112,47 @@ $latestFruit = array_pop($fruit);
 echo "Latest fruit removed: " . $latestFruit . "\n";
 print_r($latestFruit); 
 ?>
+
+
+<?php
+$array1 = array("apple","banana");
+$array2 = array("orange", "grape");
+$mergedArray = array_merge($array1, $array2);
+print_r($mergedArray)
+?>
+
+
+
+<?php 
+$color = array("Red","Green","Yellow");
+echo $color[0];
+echo $color[1];
+echo $color[2];
+?>
+
+
+<?php
+$person = array("name" => "Jhon", "age" => 30, "city" => "Yangon");
+echo $person['name'];
+echo $person['city'];
+echo $person['age'];
+?>
+
+<?php
+$matrix = array(
+    array(1, 2, 3),
+    array(4, 5, 6),
+    array(7, 8, 9)
+);
+echo $matrix[0][1];
+echo $matrix[1][2];
+?>
+
+
+<?php
+$fruit = array("Apple", "color" => "red", "Banana", "Orange" => "orange");
+echo $fruit[0];
+echo $fruit['color']
+?>  
+
+
